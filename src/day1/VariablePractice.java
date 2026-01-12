2c8c46da9ade0fb8b10a8576b75ac4cc5c041adb
Posey <poseybutter@gmail.com>
2026-01-13 00:00:58 +0900

    Day1 Java practice

---
+++ src/day1/VariablePractice.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        String name = "Lee posey";
+        int age = 31;
+        double height = 170.8;
+        boolean isStudent = false;
+        // 논리 타입에 대한 값을 저장하는 변수 이름
+        // isStudent?
+        // hasValue?
+        // isSuccess?
+        char grade = 'A';
+
+        System.out.println("=== 학생 정보 ==="); // 출력만 하는 출력 메서드
+        System.out.println("이름: " + name); // 출력 후 줄바꿈 출력 메서드
+        System.out.println("나이: " + age); // 출력 후 줄바꿈 출력 메서드
+        System.out.println("키: " + height); // 출력 후 줄바꿈 출력 메서드
+        System.out.println("학생 여부: " + isStudent); // 출력 후 줄바꿈 출력 메서드
+        System.out.println("학점: " + grade); // 출력 후 줄바꿈 출력 메서드
+    }
+}
